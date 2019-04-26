de5ff3521fcb906dd3fab794fd6b259db3750050
Sergey S <shooshpanov@yandex.com>
2019-04-26 11:57:11 -0400

    Model implemention

--- model.go
+++ model.go
@@ -2,31 +2,68 @@ package main
 
 import (
 	"database/sql"
-	"errors"
+	"fmt"
 )
 
 type user struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 func (u *user) getUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+	statement := fmt.Sprintf("SELECT name, age FROM users WHERE id = %d", u.ID)
+	return db.QueryRow(statement).Scan(&u.Name, &u.Age)
 }
 
 func (u *user) updateUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+	statement := fmt.Sprintf("UPDATE users SET name = '%s', age = %d WHERE id = %d", u.Name, u.Age, u.ID)
+	_, err := db.Exec(statement)
+	return err
 }
 
 func (u *user) deleteUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+	statement := fmt.Sprintf("DELETE FROM users WHERE id = %d", u.ID)
+	_, err := db.Exec(statement)
+	return err
 }
 
 func (u *user) createUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+	statement := fmt.Sprintf("INSERT INTO users(name, age) VALUES('%s', %d", u.Name, u.Age)
+	_, err := db.Exec(statement)
+
+	if err != nil {
+		return err
+	}
+
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&u.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getUsers(db *sql.DB, start, count int) ([]user, error) {
-	return nil, errors.New("Not implemented")
+	statement := fmt.Sprintf("SELECT id, name, age FROM users ORDER BY id OFFSET %d ROWS FETCH NEXT %d ROWS ONLY", start, count)
+	rows, err := db.Query(statement)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users := []user{}
+
+	for rows.Next() {
+		var u user
+		if err := rows.Scan(&u.ID, &u.Name, &u.Age); err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	return users, nil
 }
